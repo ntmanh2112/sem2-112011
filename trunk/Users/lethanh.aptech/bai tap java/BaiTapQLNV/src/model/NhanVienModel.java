b2c749e369286fc3ac94bea63ee5b92a78f4bb12
lethanh.aptech@gmail.com <lethanh.aptech@gmail.com@fc00a561-7660-375e-ec4c-4519a6936bde>
2011-12-12 13:31:09 +0000

    bai tap quang ly de an nhan vien

---
+++ trunk/Users/lethanh.aptech/bai tap java/BaiTapQLNV/src/model/NhanVienModel.java
@@ -0,0 +1,77 @@
+package model;
+
+public class NhanVienModel {
+	public String getManv() {
+		return manv;
+	}
+	public void setManv(String manv) {
+		this.manv = manv;
+	}
+	public String getHonv() {
+		return honv;
+	}
+	public void setHonv(String honv) {
+		this.honv = honv;
+	}
+	public String getTenlot() {
+		return tenlot;
+	}
+	public void setTenlot(String tenlot) {
+		this.tenlot = tenlot;
+	}
+	public String getTennv() {
+		return tennv;
+	}
+	public void setTennv(String tennv) {
+		this.tennv = tennv;
+	}
+	public String getNgsinh() {
+		return ngsinh;
+	}
+	public void setNgsinh(String ngsinh) {
+		this.ngsinh = ngsinh;
+	}
+	public String getDiachi() {
+		return diachi;
+	}
+	public void setDiachi(String diachi) {
+		this.diachi = diachi;
+	}
+	public String getPhai() {
+		return phai;
+	}
+	public void setPhai(String phai) {
+		this.phai = phai;
+	}
+	public String getLuong() {
+		return luong;
+	}
+	public void setLuong(String luong) {
+		this.luong = luong;
+	}
+	public String getManql() {
+		return manql;
+	}
+	public void setManql(String manql) {
+		this.manql = manql;
+	}
+	public String getPhg() {
+		return phg;
+	}
+	public void setPhg(String phg) {
+		this.phg = phg;
+	}
+	private String manv;
+	private String honv;
+	private String tenlot;
+	private String tennv;
+	private String ngsinh;
+	private String diachi;
+	private String phai;
+	private String luong;
+	private String manql;
+	private String phg;
+	 
+    
+
+}
